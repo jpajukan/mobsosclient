5c934292159036180b002f9ef4916633fa9450d6
jpajukan <jpajukan@gmail.com>
2017-04-17 22:45:17 +0300

    Playing with multiline markers on map

--- MSclient/app/src/main/java/fi/oulu/mobisocial/msclient/MapsActivity.java
+++ MSclient/app/src/main/java/fi/oulu/mobisocial/msclient/MapsActivity.java
@@ -1,15 +1,22 @@
 package fi.oulu.mobisocial.msclient;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -33,7 +40,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mDrawerList = (ListView)findViewById(R.id.navList);
 
         addDrawerItems();
+
+
     }
+
 
     private void addDrawerItems() {
         String[] osArray = { "shit", "anothershit", "asdfasdf", "nooooo!", "qwerty" };
@@ -58,6 +68,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+
+        LatLng sydney2 = new LatLng(-33, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney2).title("second in sydney klasjdflk aslkfdj sljafsöljsaö fjasdölkf l aklsdj flkasj dflsaj dflkj asdölkf jsalödfj salködfj lköasjf dlksaj flkjs fdlkj ").snippet("ksadflkasjdlkf sakldjf lk" + "\n" + "asj fdlkj aslkdfj alskjdflkasj dflkj salkjflkas fdlk aslkfj lkas" + "\n" + "fdj lksfdj lk"));
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+        //got from stackoveflow
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+            @Override
+            public View getInfoWindow(Marker arg0) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+
+                LinearLayout info = new LinearLayout(getApplicationContext());
+                info.setOrientation(LinearLayout.VERTICAL);
+
+                TextView title = new TextView(getApplicationContext());
+                title.setTextColor(Color.BLACK);
+                title.setGravity(Gravity.CENTER);
+                title.setTypeface(null, Typeface.BOLD);
+                title.setText(marker.getTitle());
+
+                TextView snippet = new TextView(getApplicationContext());
+                snippet.setTextColor(Color.GRAY);
+                snippet.setText(marker.getSnippet());
+
+                info.addView(title);
+                info.addView(snippet);
+
+                return info;
+            }
+        });
     }
 }
